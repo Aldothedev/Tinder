729c6cb80ccacdeb61f64f3f02296cc4b37347b2
Miguels <adolfolaguna007@gmail.com>
2021-09-20 20:31:51 -0400

    Updates

---
+++ precache-manifest.9928c05c49872ae01d322de692d97957.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf1b86f32fbada80776ff11a960c260",
+    "url": "/Tinder/index.html"
+  },
+  {
+    "revision": "581c02b6f4ef94681634",
+    "url": "/Tinder/static/css/main.075724a0.chunk.css"
+  },
+  {
+    "revision": "ebe8cd22d27fa4fd5f91",
+    "url": "/Tinder/static/js/2.259a7a67.chunk.js"
+  },
+  {
+    "revision": "581c02b6f4ef94681634",
+    "url": "/Tinder/static/js/main.44783e4e.chunk.js"
+  },
+  {
+    "revision": "ba002f3d6d0797ba0ab7",
+    "url": "/Tinder/static/js/runtime~main.2032a708.js"
+  }
+]);
